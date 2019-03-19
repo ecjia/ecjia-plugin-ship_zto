52a2ad84569a4eba4d724f90781bf15a97a93c5d
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-19 17:29:04 +0800

    提取插件ship_zto语言包模板

--- config.php
+++ config.php
@@ -63,6 +63,19 @@ return array(
 			array('name' => 'free_money', 'type' => 'text', 'value' => 0), /* 免费额度  */
 			array('name' => 'pay_fee', 'type' => 'text', 'value' => 0), /* 货到付款支付费用  */
 	),
+
+    'dynamic_option' => array(
+        'item_fee' 		            => __('单件商品费用：', 'ship_zto'),
+        'base_fee' 		            => __('首重费用：', 'ship_zto'),
+        'step_fee' 		            => __('续重每1000克或其零数的费用：', 'ship_zto'),
+        'pay_fee'                   => __('货到付款支付费用：', 'ship_zto'),
+        'free_money'                => __('免费额度：', 'ship_zto'),
+        'fee_compute_mode'          => __('费用计算方式：', 'ship_zto'),
+        'fee_compute_mode_range'    => array(
+            'by_weight' => __('按重量', 'ship_zto'),
+            'by_number' => __('按件数', 'ship_zto')
+        ),
+    ),
 );
 
 // end
