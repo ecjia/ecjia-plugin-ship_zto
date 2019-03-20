f436202f5641ff8ae8933e31596460903e436be1
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-03-20 19:15:42 +0800

    更新配送方式模板参数

--- templates/shipping_print.lbi.php
+++ templates/shipping_print.lbi.php
@@ -11,7 +11,7 @@
                     <td style="width:2cm; height:0.8cm;">&nbsp;</td>
                     <td style="width:2.7cm;">{$shop_name}</td>
                     <td style="width:1.2cm;">&nbsp;</td>
-                    <td>{$province}</td>
+                    <td>{$shop_province}</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
@@ -23,7 +23,7 @@
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
-                    <td style="height:1.2cm;">{$service_phone}</td>
+                    <td style="height:1.2cm;">{$shop_tel}</td>
                     <td>&nbsp;</td>
                     <td>&nbsp;</td>
                 </tr>
@@ -33,13 +33,13 @@
             <table width="100%" border="0" cellspacing="0" cellpadding="0">
                 <tr>
                     <td style="width:2cm; height:0.8cm;">&nbsp;</td>
-                    <td style="width:2.7cm;">{$order.consignee}</td>
+                    <td style="width:2.7cm;">{$customer_name}</td>
                     <td style="width:1.2cm;">&nbsp;</td>
-                    <td>{$order.region}</td>
+                    <td>{$customer_district}</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
-                    <td colspan="3" style="height:1.6cm;">{$order.address}</td>
+                    <td colspan="3" style="height:1.6cm;">{$customer_address}</td>
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
@@ -47,9 +47,9 @@
                 </tr>
                 <tr>
                     <td>&nbsp;</td>
-                    <td style="height:1.2cm;">{$order.mobile}</td>
+                    <td style="height:1.2cm;">{$customer_mobel}</td>
                     <td>&nbsp;</td>
-                    <td>{$order.zipcode}</td>
+                    <td>{$customer_post}</td>
                 </tr>
             </table>
         </td>
